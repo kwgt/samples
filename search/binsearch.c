819b892057b83af7528ce615c60f3340d717e747
Hiroshi Kuwagata <kgt9221@gmail.com>
2019-05-23 07:08:15 +0900

    ソースコードを追加。

---
+++ search/binsearch.c
@@ -0,0 +1,30 @@
+﻿int
+binsearch(double* a, size_t n, double v)
+{
+  int l;
+  int r;
+  int ret;
+
+  l = 0;
+  r = n - 1;
+
+  while (1) {
+    ret = (l + r) / 2;
+
+    if (r <= l) break;
+
+    if (a[ret] < v) {
+      l = ret + 1;
+      continue;
+    } 
+    
+    if (a[ret] > v) {
+      r = ret - 1;
+      continue;
+    }
+
+    break;
+  }
+
+  return ret;
+}
---
+++ sort/combsort.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define SHRINK(n)       ((n * 10) / 13)
+#define SWAP(a,b)       do {double t; t = b; b = a; a = t;} while(0)
+
+
+void
+combsort11(double* a, size_t n)
+{
+  int h;
+  int f;
+  int i;
+
+  /*
+   * sort by ascending order
+   */
+
+  h = n;
+
+  while (h > 1 || f) {
+    f = 0;
+    h = SHRINK(h);
+
+    if (h == 9 || h == 10) h = 11;
+
+    for (i = 0; i < ((int)n - h); i++) {
+      if (a[i] > a[i + h]) {
+        SWAP(a[i], a[i + h]);
+        f = !0;
+      }
+    }
+  }
+}
+
